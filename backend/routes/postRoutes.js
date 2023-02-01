475a091e276c660df43b1f011f40d56e7af915f0
Felix Ase <felixasejunior@gmail.com>
2023-02-01 10:04:22 +0300

    initial commit

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,42 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../mongodb/models/post.js";
+
+dotenv.config();
+
+const router = express.Router();
+
+// router.route("/").get((req, res) => {
+//   res.status(200).json({ message: "Hello from Posts" });
+// });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Get all posts
+router.route("/").get(async (req, res) => {});
+
+// Create a post
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, photo, prompt } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      photo: photoUrl.url,
+      prompt,
+    });
+
+    res.status(201).json({ data: newPost, success: true });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+export default router;
